d3e43cc7c4fcb7d953f6e881ef02249005119190
Maria Preciado <35576230+preciado20maria@users.noreply.github.com>
2018-03-06 16:55:35 -0600

    :heart:
    Co-Authored-By: YulissaDeJesus23645 <yulissadejesus23645@users.noreply.github.com>

--- Services/SeguridadService.cs
+++ Services/SeguridadService.cs
@@ -4,10 +4,29 @@ namespace CesarWeb.Services
     public class SeguridadService : ISeguridadService<int>
     {
         static string ABECEDARIO = ("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ");
+        static string abecedario = ("abcdefghijklmnñopqrstuvwxyz");
 
         ///  Aquí deben hacer todo el código necesario para Desencriptar el mensaje
         public string DesEncriptar(string Mensaje, int clave)
         {
+            int i;
+            int j;
+            string nuevo= Mensaje;
+            for(i=0; i<Mensaje.Length; i++)
+            {
+                for(j=0; j<26; j--)
+                {
+                    if(Mensaje [i] == ABECEDARIO[j])
+                    {
+                        nuevo = nuevo.Replace(nuevo[i], ABECEDARIO[j-clave%27]);
+                    }
+                    if(Mensaje[i] == abecedario[j])
+                        {
+                            nuevo = nuevo.Replace(nuevo[i], abecedario[j-clave%27]);
+                        }
+                }
+            }
+        Mensaje = nuevo;
             return Mensaje;
         }
 
@@ -16,6 +35,30 @@ namespace CesarWeb.Services
         ///  Aquí deben hacer todo el código necesario para Encriptar el mensaje
         public string Encriptar(string Mensaje, int clave)
         {
+         int i;
+         int j;
+          string nuevo=Mensaje;
+	        for(i=0; i < Mensaje.Length ; i++)
+
+                {  
+                    for(j = 0; j < 26; j++)
+                    {
+                        if(Mensaje[i] == ABECEDARIO[j])
+                        {
+                
+                    nuevo = nuevo.Replace(nuevo[i], ABECEDARIO[j+clave%27]);
+                            
+                        }
+
+                        if(Mensaje[i] == abecedario[j])
+                        {
+                            nuevo = nuevo.Replace(nuevo[i], abecedario[j+clave%27]);
+                        }
+                    }
+
+
+	            }
+            Mensaje=nuevo;
             return Mensaje;
         }
     }
